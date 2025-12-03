f95907f38e2e179ac35bc414a92ee66f9a8cacf9
Wan Muzaffar <wanmuz86@gmail.com>
2025-12-03 16:42:19 +0800

    final code day 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 // cross check this file with main.js 
 // almost the same function, render whatever in App component in root div
-const { createRoot } = require("react-dom/client");
+import React from 'react';
+import { createRoot } from "react-dom/client";
 import App from './App';
 
 const container = document.getElementById("root")
+
 if (!container){
     throw new Error("Root container not available");
 }
 
-const root = createRoot(contaner);
+const root = createRoot(container);
 root.render(<App/>)
